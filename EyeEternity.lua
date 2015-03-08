5222b9410efe8407639ed1a6803268de6e6f9946
stassart <stassart@0e97db43-3fc7-49fc-aa1d-61b5264851ff>
2015-03-08 08:57:40 +0000

    Updated Eye of Eternity debuff flag.

--- EyeEternity.lua
+++ EyeEternity.lua
@@ -1,5 +1,6 @@
 -- local zone = "The Eye of Eternity"
 local zoneid = 527
+-- Note: confirmed spell IDs as of WoD 6.1 3/8/2015
 
 -- Check Compatibility
 if GridStatusRD_OldRaids.rd_version < 502 then
@@ -11,5 +12,5 @@ end
 --Malygos
 GridStatusRaidDebuff:BossNameId(zoneid, 10, "Malygos")
 GridStatusRaidDebuff:DebuffId(zoneid, 57407, 11, 5, 5) --Surge of Power
-GridStatusRaidDebuff:DebuffId(zoneid, 56272, 12, 5, 5) --Arcane Breath
+GridStatusRaidDebuff:DebuffId(zoneid, 56272, 12, 5, 5, true ) --Arcane Breath
 
